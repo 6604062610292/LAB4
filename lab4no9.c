46c8552c42cbe6026bfa83462be406d865f3e499
ชานนท์ สายวิสัย <s6604062610292@email.kmutnb.ac.th>
2026-01-04 18:58:02 +0700

    Add files via upload

---
+++ lab4no9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+	
+int score,i;
+
+while(score!= -1){
+
+scanf("%d",&score);
+if(score == -1){
+ return 1;
+  } 
+
+if (score >= 68){
+	if (score >= 85){
+		printf("%d(A)\n",score);
+	}else if (score >= 75){
+		printf("%d(B)\n",score);
+	}else{
+printf("%d(C)\n",score);
+ }
+}else{
+	if (score >= 55){
+		printf("%d(D)\n",score);
+	}else{
+		printf("%d(F)\n",score);
+	}
+  }
+}
+return 0;
+};

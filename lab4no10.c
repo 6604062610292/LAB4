e03921bac33731cb1cc976c2be43fc1ba0507404
ชานนท์ สายวิสัย <s6604062610292@email.kmutnb.ac.th>
2026-01-04 19:14:03 +0700

    Add files via upload

---
+++ lab4no10.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    
+    int countA,countB,countC,countD=0,countF=0;
+
+    while (1) {
+        scanf("%d", &score);
+        if (score == -1) {
+            break; 
+        }
+        if (score >= 68) {
+            if (score >= 85) {
+                printf("%d(A)\n", score);
+                countA++; 
+            } else if (score >= 75) {
+                printf("%d(B)\n", score);
+                countB++; 
+            } else {
+                printf("%d(C)\n", score);
+                countC++; 
+            }
+        } else {
+            if (score >= 55) {
+                printf("%d(D)\n", score);
+                countD++; 
+            } else {
+                printf("%d(F)\n", score);
+                countF++; 
+            }
+        }
+    }
+
+    printf("A(%d)\n", countA);
+    printf("B(%d)\n", countB);
+    printf("C(%d)\n", countC);
+    printf("D(%d)\n", countD);
+    printf("F(%d)\n", countF);
+
+    return 0;
+}
